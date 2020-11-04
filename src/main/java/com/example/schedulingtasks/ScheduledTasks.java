663bc7c562e5c6ff8508d73b75cd7f0064c11a7e
Christian Schulz <trutty3@gmail.com>
2020-11-04 15:52:02 +0100

    Log fixed log message (1024 bytes); make TAGS configurable

--- src/main/java/com/example/schedulingtasks/ScheduledTasks.java
+++ src/main/java/com/example/schedulingtasks/ScheduledTasks.java
@@ -9,9 +9,9 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+
+import static com.example.schedulingtasks.SchedulingTasksApplication.TAGS;
 
 @Component
 @EnableAsync
@@ -19,47 +19,15 @@ public class ScheduledTasks {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledTasks.class);
 
-    private static final Runnable[] RUNNABLES;
-
-    static {
-        final List<Runnable> runnables = new ArrayList<>();
-        runnables.add(ScheduledTasks::log);
-
-        if (SchedulingTasksApplication.GENERATE_EXCEPTION_LOGS) {
-            runnables.add(ScheduledTasks::throwException);
-        }
-
-        RUNNABLES = runnables.toArray(new Runnable[0]);
-    }
-
-    private static final String[] TAGS;
-
-    static {
-        final List<String> tags = new ArrayList<>();
-        tags.add("INFO");
-
-        if (SchedulingTasksApplication.GENERATE_SECURITY_LOGS) {
-            tags.add("SECURITY");
-        }
-
-        TAGS = tags.toArray(new String[0]);
-    }
+    // Creates log message which is 1024 bytes long in JSON format
+    private static final String LOG_MESSAGE = "Lorem ipsum dolor sit amet consectetur adipiscing elit taciti ultrices, cum potenti ridiculus leo varius porta fermentum. Turpis a habitant vivamus integer sollicitudin nulla torquent, litora ridiculus etiam ullamcorper porta aliquet consequat sed, posuere imperdiet orci varius curabitur montes. Ad molestie et curae felis eu sed mi lacinia euismod, vel consequat inceptos nascetur penatibus ultricies mauris natoque. Cras proin fames blandit gravida consequat tincidunt vivamus malesuada torquent, curabitur ante risus luctus nibh dapibus vestibulum duis senectus, class volutpat integer mus pulvinar euismod pharetra fermentum. Imperdiet ullamcorper cubilia tortor phasellus laoreet augue placerat rhoncus netus, integer congue tempor dapibus eget senectus tempus elementum habitant, etiam aenean auctor snectus tempus";
 
     @Async
     @Scheduled(fixedRateString = "${LOG_RATE_IN_MILLISECONDS}")
     public void scheduled() {
-        RUNNABLES[ThreadLocalRandom.current().nextInt(0, RUNNABLES.length)].run();
-    }
-
-    public static void log() {
         String tag = TAGS[ThreadLocalRandom.current().nextInt(0, TAGS.length)];
         StructuredArgument s = StructuredArguments.v("tags", new String[]{tag});
-
-        LOGGER.info("Just logging some info message with any content to have some data and tag '{}'", s);
-    }
-
-    public static void throwException() {
-        throw new ArithmeticException("My test RuntimeException without explicit logging");
+        LOGGER.info("{} {}", LOG_MESSAGE, s);
     }
 
 }
--- src/main/java/com/example/schedulingtasks/SchedulingTasksApplication.java
+++ src/main/java/com/example/schedulingtasks/SchedulingTasksApplication.java
@@ -2,30 +2,35 @@ package com.example.schedulingtasks;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableScheduling
 public class SchedulingTasksApplication {
 
-    public static final boolean GENERATE_SECURITY_LOGS = getEnvWithDefault("GENERATE_SECURITY_LOGS", true);
+    public static final String[] TAGS;
 
-    public static final boolean GENERATE_EXCEPTION_LOGS = getEnvWithDefault("GENERATE_EXCEPTION_LOGS", true);
+    static {
+        final String tags = System.getenv("TAGS");
+        if (tags != null) {
+            TAGS = Arrays.stream(tags.split(","))
+                    .map(String::trim)
+                    .distinct()
+                    .toArray(String[]::new);
+        } else {
+            TAGS = new String[]{""};
+        }
+    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SchedulingTasksApplication.class);
 
-    private static boolean getEnvWithDefault(final String env, final boolean defaultValue) {
-        final String value = System.getenv(env);
-        return value != null ? Boolean.parseBoolean(value) : defaultValue;
-    }
-
     public static void main(String[] args) {
         LOGGER.info("Running with LOG_RATE_IN_MILLISECONDS: " + System.getenv("LOG_RATE_IN_MILLISECONDS"));
-        LOGGER.info("Running with GENERATE_SECURITY_LOGS: " + GENERATE_SECURITY_LOGS);
-        LOGGER.info("Running with GENERATE_EXCEPTION_LOGS: " + GENERATE_EXCEPTION_LOGS);
+        LOGGER.info("Running with TAGS: " + Arrays.toString(TAGS));
         SpringApplication.run(SchedulingTasksApplication.class);
     }
 }
